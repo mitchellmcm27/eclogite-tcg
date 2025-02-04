a8ecd518c4c7656f0b835ead59b2db6df9a5c0e7
Mitchell McMillan <mitchellmcm27@gmail.com>
2025-02-04 20:20:00 +0000

    build reactions inside dockerfile

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#chgrp adm -R .
-#chmod g+w+x -R .
-# Generate and build reactions
-
-cd tcg_slb_database
-./scripts/generate_reactions_eclogite -v slb21
-./scripts/build_reactions database/reactions/eclogitization_2024_slb21_rx.rxml
